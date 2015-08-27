949bafa3ed847f022b3d6e384bb0f3b6da472ffe
Эльф <deadelf79@gmail.com>
2015-08-27 20:39:39 +0300

    Генератор героев
    Закончен генератор героев
    TODO Дописать equips, когда будет готово

--- code/module.Redenik.class.self.rb
+++ code/module.Redenik.class.self.rb
@@ -2,7 +2,7 @@
 
 module Redenik
 	class << self
-		START___MAX_ACTORS = 10
+		START___MAX_ACTORS = 30
 		STATS___CLASSES = [
 			{:class_name=>:citizen,		:st=>10,	:dx=>10,	:iq=>10,	:ht=>10},
 			{:class_name=>:warrior,		:st=>12,	:dx=>9,		:iq=>8,		:ht=>11},
@@ -30,19 +30,33 @@ module Redenik
 
 		private
 		def _gen_actors
-			START___MAX_ACTORS.times{
+			new_level = 0
+			START___MAX_ACTORS.times{|index|
 				rand_class = STATS___CLASSES[STATS___CLASSES.keys.sample]
 				@game_actors << Redenik::Actor.new(
-					Redenik::NameGen.make_name(3,4),
-					rand_class[:class_name],
-					[rand_class[:st],rand_class[:dx],rand_class[:iq],rand_class[:ht]],
-					
+					Redenik::NameGen.make_name(3,4),									# NAME
+					rand_class[:class_name],											# APPEARANCE
+					[rand_class[:st],rand_class[:dx],rand_class[:iq],rand_class[:ht]],	# STATS
+					[],																	# EQUIPS
+					new_level+=(index.to_f/6).to_i+1 									# LEVEL
 				)
 			}
 		end
-		def _gen_items;end
-		def _gen_weapons;end
-		def _gen_armors;end
-		def _gen_skills;end
+
+		def _gen_items
+
+		end
+
+		def _gen_weapons
+
+		end
+
+		def _gen_armors
+
+		end
+
+		def _gen_skills
+
+		end
 	end
 end
