5731e92f557305d0f8dedd178139fc9bd2d4f0ec
Эльф <deadelf79@gmail.com>
2015-09-14 10:57:57 +0300

    Ошибка #4
    См. ошибки:
    - https://github.com/deadelf79/Redenik/commit/44b48295d78a466515f70f0d6f9bb8ed568fee23
    - https://github.com/deadelf79/Redenik/commit/895cd67c2d8726fb686e6ebf8d772932394755cb
    - ошибку #1 я потерял Т_Т
    
    В дополнение к этому: module вместо class

--- code/module.Redenik.class.Weapon.rb
+++ code/module.Redenik.class.Weapon.rb
@@ -1,11 +1,11 @@
 # encoding utf-8
 
-module Redenik::Weapon < BasicItem
+class Redenik::Weapon < Redenik::BasicItem
 	def initialize(effects,rarity,weapon_type)
-		@health 	= _gen_health_by_rare(rarity)
-		@mana 		= _gen_mana_by_rare(rarity)
-		@dual_wield = _gen_wield_by_type(weapon_type)
-		@start_price= Redenik::Balance::write_here_something
+		@health 		= _gen_health_by_rare(rarity)
+		@mana 			= _gen_mana_by_rare(rarity)
+		@dual_wield 	= _gen_wield_by_type(weapon_type)
+		@start_price	= Redenik::Balance::write_here_something
 		super(@health,@mana,effects,rarity,start_price,:weapon)
 	end
 
