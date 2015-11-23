d66ceecefa7cff79fe34bbe191bcd6eed2d9d827
Эльф <deadelf79@gmail.com>
2015-11-24 02:35:11 +0300

    Метод update для базового меню
    И почему я забыл о нем в прошлый раз?

--- code/module.Redenik.GameManager.class.Screen_Menu_Base.rb
+++ code/module.Redenik.GameManager.class.Screen_Menu_Base.rb
@@ -1,6 +1,12 @@
 # encoding utf-8
 
 class Redenik::GameManager::Screen_Menu_Base < Redenik::GameManager::Screen_Base
+	def update
+		super
+		fire___ok if Input.trigger?(:C)
+		fire___cancel if Input.trigger?(:B)
+	end
+
 	def fire___ok
 		#
 	end
