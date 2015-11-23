efd587ca811e970ef55d31646d443d32ef4e1d56
Эльф <deadelf79@gmail.com>
2015-11-24 02:58:14 +0300

    Добавлен менеджер плагинов
    Он пока пустой, я расширю его в будущем.

---
+++ code/module.Redenik.PluginManager.rb
@@ -0,0 +1,20 @@
+# encoding utf-8
+module PluginManager
+	class << self
+		def enabled?
+			#
+		end
+
+		def read_list
+
+		end
+
+		def check_for_errors
+
+		end
+
+		def load
+			#
+		end
+	end
+end
--- code/module.Redenik.rb
+++ code/module.Redenik.rb
@@ -579,6 +579,15 @@ module Redenik
 	end
 
 	module Balance;end
+
+	module PluginManager
+		class << self
+			def enabled?;end
+			def read_list;end
+			def check_for_errors;end
+			def load;end
+		end
+	end
 end
 
 module Kernel
