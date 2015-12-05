dafb1cbba85b96666d4482bdd0f8511f2b264143
Эльф <deadelf79@gmail.com>
2015-12-05 09:44:23 +0300

    База для экрана ачивок
    Пока не тестировал. Возможны ужасные ошибки и их последствия. Я предупреждал.

--- code/module.Redenik.GameManager.class.Screen_Achievements.rb
+++ code/module.Redenik.GameManager.class.Screen_Achievements.rb
@@ -22,7 +22,21 @@ class Redenik::GameManager::Screen_Achievements < Redenik::GameManager::Screen_M
 	end
 
 	def create___game_achi_window
-
+		_load_names
+		@achi_window = Redenik::Graphics::Window.new(
+			300,
+			300,
+			192,
+			192
+		)
+		with @title_window do
+			@achis.each_keys{|achi|
+				add_button(@achis[achi][:view][:name],:fire___show_achiv_desc)
+			}
+			activate
+		end
+		@title_window.z = 1
+		@title_window.refresh
 	end
 
 	def fire___change_achiv_to_show(index)
@@ -30,6 +44,16 @@ class Redenik::GameManager::Screen_Achievements < Redenik::GameManager::Screen_M
 	end
 
 	def fire___show_achiv_desc(index)
+
+	end
+
+	private
+
+	def _load_names
+		@achis = JSON.decode(open("data/achievements.json","r"))
+	end
+
+	def _load_data
 
 	end
 end
