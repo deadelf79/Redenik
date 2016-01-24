78bd0b3214db9592ace15f47134301592cb1a11d
Эльф <deadelf79@gmail.com>
2016-01-24 17:45:22 +0300

    Загрузка статичной карты
    Теперь статичная карта грузится не из кода, а из файла.
    Загрузка происходит только из строк между тегами begindata и enddata.

--- code/module.Redenik.Graphics.class.Static_Map.rb
+++ code/module.Redenik.Graphics.class.Static_Map.rb
@@ -3,17 +3,23 @@
 class Redenik::Graphics::Static_Map < Redenik::Graphics::Map
 	def initialize(id,filename)
 		file = open('data/maps/'+filename+'.static_map',"rb")
-		super(id,width,height,type,min_level,max_level)
+		super( id, 11, 11, :safe, 1, 2 )
 
-		@data = Table.new(10,10)
-		(1...10).each do |index|
-			@data[index,1] = 2
-			@data[index,2] = 2
-			@data[index,5] = 2
-			@data[index,9] = 2
-			@data[1,index] = 2
-			@data[5,index] = 2
-			@data[9,index] = 2
-		end
+		start_read = false
+		@data.clear
+
+		file.readlines.each{|line|
+			if line=~/begindata/
+				start_read = true
+			elsif line=~/enddata/
+				start_read = false
+				break
+			end
+			if start_read
+				index = @data.add_line
+				@data[index] = line
+			end
+		}
+		self
 	end
 end
