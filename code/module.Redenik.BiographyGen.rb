a0981bc645ceb12b73aadc35900d5a5735fda36a
Эльф <deadelf79@gmail.com>
2016-03-09 13:37:19 +0300

    Добавлен модуль BiographyGen
    Пока что без текста и жутко наивный. После того, как я недавно прочитал про гендер более подробно, серьезно пересмотрю здешнюю механику.

---
+++ code/module.Redenik.BiographyGen.rb
@@ -0,0 +1,45 @@
+﻿# encoding utf-8
+
+module BiographyGen
+	class << self
+		def prepare
+			@data = {
+				name:"Василий",
+				age:18,
+				sex:(:male),
+				gender:(:getero),
+				surname:"Пупкин",
+				secondname:"Иванович",
+				orphan:false,
+				widower:false,
+				childrens:false,
+			}
+		end
+
+		def actor=(value)
+			@data[:name] 		= value.name
+			@data[:age] 		= value.age
+			@data[:sex] 		= value.sex
+			@data[:gender] 		= value.gender
+			@data[:surname] 	= value.surname
+			@data[:secondname] 	= value.secondname
+		end
+
+		def orphan=(value)
+			@data[:orphan] = value
+		end
+
+		def widower=(value)
+			@data[:widower] = value
+		end
+
+		def childrens=(value)
+			@data[:childrens] = value
+		end
+
+		def generate
+			story = []
+			return story.join("\s")
+		end
+	end
+end
