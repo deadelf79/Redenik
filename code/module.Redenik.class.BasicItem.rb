cacf906ca96d6310a75f4f44ac9a1bc63a30f1e2
Эльф <deadelf79@gmail.com>
2016-03-23 17:51:50 +0300

    Загрузка иконок по типу предмета
    Тестовая версия, пока нет иконок

--- code/module.Redenik.class.BasicItem.rb
+++ code/module.Redenik.class.BasicItem.rb
@@ -12,6 +12,8 @@ class Redenik::BasicItem < Redenik::Basic
 		}
 		_load_icon_by_type
 		_gen_help_info
+
+		@disposed = false
 	end
 
 	def help_info=(new_text)
@@ -22,10 +24,51 @@ class Redenik::BasicItem < Redenik::Basic
 		@appearance[:icon]
 	end
 
+	def dispose
+		@disposed = true
+	end
+
+	def disposed?
+		@disposed
+	end
+
 	private
 
 	def _load_icon_by_type
-		#
+		item_fx = []
+		@effects.each{|fx|
+			case fx
+			when :heal_hp
+				item_fx += ["hp"]
+			when :heal_mp
+				item_fx += ["mp"]
+			when :food
+				item_fx += ["food"]
+			when :poison
+				item_fx += ["poison"]
+			when :rotten
+				item_fx += ["rotten"]
+			end
+		}
+
+		# все иконки расположены в папке Gfx/Icons/items
+		# Если предмет - еда
+		if item_fx.include? "food"
+			if item_fx.include? "hp"
+				@appearance[:icon] = "healing_food"
+			elsif item_fx.include? "mp"
+				@appearance[:icon] = "mana_food"
+			elsif item_fx.include? "poison"
+				@appearance[:icon] = "poisoned_food"
+			elsif item_fx.include? "rotten"
+				@appearance[:icon] = "rotten_food"
+			else
+				@appearance[:icon] = "food_only"
+			end
+		# Если предмет - бутылочка с жидкостью
+		else
+			@appearance[:icon] = "random_bottle_#{rand(8)}"
+		end
 	end
 
 	def _gen_help_info
