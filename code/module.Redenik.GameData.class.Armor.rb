499f034decc5f47ebaafd9cc92cb778a0312ad0b
Эльф <deadelf79@gmail.com>
2016-06-06 11:43:13 +0300

    Фикс родства класса брони
    вообще, надо бы подобные фиксы оставить на потом. прежде надо бы с итемами разобраться, а потом уж в остальное лезть.

--- code/module.Redenik.GameData.class.Armor.rb
+++ code/module.Redenik.GameData.class.Armor.rb
@@ -1,6 +1,6 @@
 #encoding utf-8
 
-class Redenik::Armor < Redenik::BasicItem
+class Redenik::GameData::Armor < Redenik::BasicItem
 	def initialize(effects,rarity,equip_type)
 		super(1,0,effects,rarity,0,:none)
 	end
